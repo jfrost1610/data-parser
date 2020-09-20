9b8094292b0732bad296459ebc7817301c2692e0
jfrost1610 <jobinthomas1610@gmail.com>
2020-09-20 09:56:32 +0530

    first commit

---
+++ src/main/java/com/frost/dataparser/DataParserApplication.java
@@ -0,0 +1,13 @@
+package com.frost.dataparser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataParserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataParserApplication.class, args);
+	}
+
+}
